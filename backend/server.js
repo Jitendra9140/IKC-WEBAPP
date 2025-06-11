113efac9e47dcdf89a405c64cf8e420e6c54d738
Jitendra Yadav <yadavjayup72.com>
2025-06-11 09:50:13 +0530

    Backend Cross origin setup  by Jitendra Yadav

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,27 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000', // for local React frontend
+  'https://ikc-webapp.netlify.app' // for deployed frontend
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
